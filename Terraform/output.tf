d66e961971277a8c1a91e39d4c7daa0cacf98a89
Ubuntu <ubuntu@ip-172-31-25-54.ec2.internal>
2025-09-07 17:42:37 +0000

    Added IaC for K8s

---
+++ Terraform/output.tf
@@ -0,0 +1,15 @@
+output "cluster_id" {
+  value = aws_eks_cluster.netflix.id
+}
+
+output "node_group_id" {
+  value = aws_eks_node_group.netflix.id
+}
+
+output "vpc_id" {
+  value = aws_vpc.netflix_vpc.id
+}
+
+output "subnet_ids" {
+  value = aws_subnet.netflix_subnet[*].id
+}
